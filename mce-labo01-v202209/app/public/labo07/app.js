9b958f7e18ef2d953b5214a690439ffc232d371e
Toluwani Ezekiel Akindele <toluwani.akindele@student.odisee.be>
2022-11-19 15:27:35 +0100

    changes

--- mce-labo01-v202209/app/public/labo07/app.js
+++ mce-labo01-v202209/app/public/labo07/app.js
@@ -13,10 +13,11 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(cors());
 
+let db = new sqlite3.Database('discotheque.db');
 
 // get all songs from the database
 app.get('/songs', function (req, res) {
-    let db = new sqlite3.Database('discotheque.db');
+    
 
     db.all('SELECT * FROM songs ORDER BY artist', [], (err,rows) => {
         if(err){
@@ -32,37 +33,98 @@ app.get('/songs', function (req, res) {
 
 // get one single song by id from the database
 app.get('/songs/:id', function (req, res) {
-    // const id = reg.params.id;
-    // let db = new sqlite3.Database('discotheque.db');
-    // const stmt = db.prepare("SELECT * FROM songs Where id = ?");
-    // stmt.get(id,(err,row) => {
-    //     if(err || !row){
-    //         res.status(404);
-    //     }
-    //     else{
-    //         res.json
-    //     }
-    // });
-    // // @TODO
-    // console.log("here");
-    // res.sendStatus(404);
+    var sql = "select * from songs where id = ?"
+    var params = [req.params.id]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":row
+        })
+      });
 });
 
 // add a song to the database
 app.post('/songs', function (req, res) {
-
     // @TODO
+    var errors=[]
+    if (!req.body.title){
+        errors.push("No titele specified");
+    }
+    if (!req.body.artist){
+        errors.push("No artist specified");
+    }
+    if (!req.body.bpm){
+        errors.push("No bpm specified");
+    }
+    if (!req.body.genre){
+        errors.push("No genre specified");
+    }
+    if (errors.length){
+        res.status(400).json({"error":errors.join(",")});
+        return;
+    }
+
+    var data = {
+        title : reg.params.title,
+        artist : reg.params.artist,
+        bpm : reg.params.bpm,
+        genre : reg.params.genre
+    }
+    console.log("here");
+    const stmt = "INSERT INTO songs (title, artist, bpm,genre) VALUES (?,?, ?,?)";
+    var params = [data.title, data.artist, data.bpm, data.genre];
+    db.run(stmt,params, function(err){
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data,
+            "id" : this.lastID
+        })
+    });
 });
 
 // update a song in the database
 app.put('/songs/:id', function (req, res) {
+    var data = {
+        title : reg.params.title,
+        artist : reg.params.artist,
+        bpm : reg.params.bpm,
+        genre : reg.params.genre
+    }
 
+    db.run(
+        `UPDATE songs set 
+           title = COALESCE(?,title), 
+           artist = COALESCE(?,artist), 
+           bpm = COALESCE(?,bpm) ,
+           genre = COALESCE(?,genre)
+           WHERE id = ?`,
+        [data.title, data.artist, data.bpm,data.genre, req.params.id],
+        function (err, result) {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({
+                message: "success",
+                data: data,
+                changes: this.changes
+            })
+    });
     // @TODO
 });
 
 
 
 // make sure to run the app with `node app.js` command
+    
 app.listen(port, function() {
     console.log(`Example app listening on port ${port}!`)
 });
